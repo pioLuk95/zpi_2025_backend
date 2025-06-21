58a5bdd960bedb0bbbc304a110fc8ec37bc23b2e
Dominik Nowacki <dominik.nowacki02@gmail.com>
2025-06-21 19:28:23 +0200

    naprawa 2

--- resources/views/layouts/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<footer class="footer">
-  <div class="container-fluid clearfix">
-    <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Ukradłem</span>
-    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Tylko głupi by nie ukradł
-    </span>
-  </div>
-</footer>

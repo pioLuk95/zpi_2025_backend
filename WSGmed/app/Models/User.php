980f78fdd2fbe22036975e6fec7c780c1ccb6c9c
drFiFlok <michal.swierczynski@student.wsg.pl>
2025-06-22 02:31:57 +0200

    Update User.php

--- WSGmed/app/Models/User.php
+++ WSGmed/app/Models/User.php
@@ -50,4 +50,24 @@ class User extends Authenticatable implements JWTSubject
     {
         return !empty($this->two_factor_confirmed_at);
     }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
